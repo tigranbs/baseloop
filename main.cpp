1271fcfe7af31457606195a04875ad8b604deb84
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2016-12-28 17:33:15 +0400

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ using namespace BaseLoop;
 class TestServer: public BaseLoop {
 protected:
     // callback for accepting connection here
-    void acceptable(loop_event_data *data);
+    void acceptable(loop_event_data_t *data);
     // callback for reading data from socket here
-    void readable(loop_event_data *data);
+    void readable(loop_event_data_t *data);
     // callback for writing data to socket
-    void writable(loop_event_data *data);
+    void writable(loop_event_data_t *data);
     // get notification from pipe and read commands
     void notify();
 };
